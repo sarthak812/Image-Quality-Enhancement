932d6b6d67d0302dd7044a6d8951ecbd2a5f34a5
Sarthak <sarthak812@gmail.com>
2020-07-30 14:26:52 +0530

    Image upload added

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def upload_file1():
+def enhancement():
     return render_template('Image Enhancement.html')
+
+
+app.config['Image_uploads'] = "C:\\Users\\sarth\\PycharmProjects\\Image-Quality-Enhancement\\demo"
 
 
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-        return 'file uploaded successfully'
+        if request.files:
+            image = request.files['image']
+            image.save(os.path.join(app.config['Image_uploads'], image.filename))
+            print('Image Saved')
+        return redirect(request.url)
+    return render_template('Image Enhancement.html')
+
 
 
 if __name__ == '__main__':
